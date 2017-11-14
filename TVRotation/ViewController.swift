e431d49cecdcebfc3d5527089b55dae2700abdc3
Hilton Pintor <hiltonpintor@gmail.com>
2017-11-14 06:38:50 -0300

    Initial Commit

---
+++ TVRotation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TVRotation
+//
+//  Created by Hilton Pintor Bezerra Leite on 14/11/17.
+//  Copyright © 2017 Hilton Pintor Bezerra Leite. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
